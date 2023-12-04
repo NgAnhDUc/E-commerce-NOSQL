56c7cc09a6707be525a241b79cd4ec647bcf97c3
Finn <you@example.comhdu389617@gmail.com>
2023-12-04 10:40:17 +0700

    Post New User

---
+++ server/models/user.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose'); // Erase if already required
+const { use } = require('../routes/user');
+const bcryct = require('bcrypt')
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true,
+    },
+    lastname:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        default:'user',
+    },
+    cart:{
+        type:Array,
+        default:[]
+    },
+    address:[{type:mongoose.Types.ObjectId, ref:'Address'}],
+    wishlist:[{type:mongoose.Types.ObjectId, ref:'Product'}],
+    isBlocker:{
+        type:Boolean,
+        default:false,
+    },
+    refreshToken:{
+        type:String,
+    },
+    passwordChangeAt:{
+        type:String,
+    },
+    passwordResetToken:{
+        type:String,
+    },
+    passwordResetExpires:{
+        type:String,
+    },
+},{
+    timestamps:true
+});
+// userSchema.pre('save', async function (next) {
+//     const salt = bcryct.genSaltSync(10)
+//     this.password = await bcryct.hash(this.password, salt)
+// })
+//Export the model
+module.exports = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 require('dotenv').config()
 const dbConnect= require('./config/dbconnect')
+const initRouter = require('./routes')
 
 const app =express()
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-app.use('/',(req,res) =>{res.send('SERVER ONNNNN')})
 
 dbConnect()
+initRouter(app)
 
 app.listen(port,()=>{
     console.log('Server running on the port: '+port);
